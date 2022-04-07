1981f61d7ecf6eaa8d838762b5253d0b812a9fe3
VanyaKornev2089 <94173875+VanyaKornev2089@users.noreply.github.com>
2022-04-07 14:00:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,20 +10,23 @@ const DomElement = function(selector,height,width,bg,fontSize) {
 
 DomElement.prototype.createNewElement = function() {
     let newElement;
-    if (selector[0] === '.') {
+    if (this.selector[0] === '.') {
         newElement = document.createElement('div')
         newElement.className = this.selector.slice(1)
     }
-    if (selector[0] === '#'){
+    if (this.selector[0] === '#'){
         newElement = document.createElement('p')
         newElement.id = this.selector.slice(1)
     }
-    newElement.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}`
+    newElement.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px`
     return newElement;
 }
 
 
-let elem1 = new DomElement('#text', 10, 25, 'black', 10)
-let elem2 = new DomElement('.block', 15, 30, 'green', 14)
+let elem1 = new DomElement('#text', 40, 40, 'blue', 40)
+let elem2 = new DomElement('.block', 20, 55, 'yellow', 50)
 console.log(elem1)
 console.log(elem2)
+
+document.body.append(elem1.createNewElement())
+document.body.append(elem2.createNewElement())
